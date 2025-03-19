561015945d13fd1688b69b46b5006421debf1f11
JJ0824 <auxjungjae@gmail.com>
2025-03-19 09:44:05 +0900

    Update MovieDetail.jsx

--- src/Components/MovieList/MovieDetail.jsx
+++ src/Components/MovieList/MovieDetail.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  
+  width : 100%;
+  display: flex;
+  flex-direction:column;
 `;
 const Header = styled.div`
-  
+  width: 100%;
+  color: dodgerblue;
+  display: flex;
+  justify-content: space-between;
 `;
 const Img = styled.div`
-  
+  width: 100%;
 `;
 const Content = styled.div`
-  
+  font-size: 1rem;
+  line-height: 30px;
+  color: #333;
 `;
 
 function MovieDetail() {
