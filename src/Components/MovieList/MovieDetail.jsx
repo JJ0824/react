21e14e53917dcd63fdd26c33e93ada3b619ad72e
JJ0824 <auxjungjae@gmail.com>
2025-03-19 10:26:08 +0900

    Update MovieDetail.jsx

--- src/Components/MovieList/MovieDetail.jsx
+++ src/Components/MovieList/MovieDetail.jsx
@@ -52,20 +52,23 @@ function MovieDetail() {
   return (
     <div>
       <Container>
-        <Header>
-          <h1>타이틀</h1>
-
-        </Header>
-        <Img></Img>
-        <Content>
-          <p>타이틀</p>
-          <p>장르</p>
-          <p>개봉일</p>
-          <p>상영시간</p>
-          <p>감독</p>
-          <p>배우</p>
-          <p>영화설명</p>
-        </Content>
+        {
+          loading ? "로딩 중..." : <>
+            <Header>
+              <h1>타이틀</h1>
+            </Header>
+            <Img></Img>
+            <Content>
+              <p>타이틀</p>
+              <p>장르</p>
+              <p>개봉일</p>
+              <p>상영시간</p>
+              <p>감독</p>
+              <p>배우</p>
+              <p>영화설명</p>
+            </Content>
+          </>
+        }
       </Container>
     </div>
   );
